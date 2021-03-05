d9de379f13f4aa94d33f847cb183b7dd8260b2b1
jthorton <josh.horton@newcastle.ac.uk>
2021-03-05 12:32:00 +0000

    pin pydantic due to errors

--- openff/bespokefit/forcefield_tools.py
+++ openff/bespokefit/forcefield_tools.py
@@ -87,7 +87,7 @@ class ForceFieldEditor:
                     smirk_data["id"] = current_param.id
                     # update the parameter using the init to get around conditional assigment
                     current_param.__init__(**smirk_data)
-                except KeyError:
+                except IndexError:
                     smirk_data["id"] = _smirks_ids[smirk_type] + str(no_params + i)
                     current_params.append(_smirks_conversion[smirk_type](**smirk_data))
 
