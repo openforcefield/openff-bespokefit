bd26aa8fc4e980629c774a71ff5f272009f8fa4c
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-04-26 10:53:28 +0100

    Update project to most recent layout (#20)

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     include_package_data=True,
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-    install_requires=["pydantic"]
+    install_requires=[]
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
     # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
