2207b838af2b6db213802d237d120045d82f789a
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-05-12 17:08:27 +0100

    Retrieve `ambertools` version from conda as backup (#37)

--- openff/bespokefit/utilities/provenance.py
+++ openff/bespokefit/utilities/provenance.py
@@ -1,6 +1,25 @@
+import functools
+import re
+import subprocess
 from typing import Dict, Optional
 
 import pkg_resources
+
+
+@functools.lru_cache()
+def _get_conda_list_package_versions() -> Dict[str, str]:
+    """Returns the versions of any packages found while executing `conda list`."""
+
+    list_output = subprocess.check_output(["conda", "list"]).decode().split("\n")
+
+    package_versions = {}
+
+    for output_line in list_output[3:-1]:
+
+        package_name, package_version, *_ = re.split(" +", output_line)
+        package_versions[package_name] = package_version
+
+    return package_versions
 
 
 def get_ambertools_version() -> Optional[str]:
@@ -9,8 +28,9 @@ def get_ambertools_version() -> Optional[str]:
     try:
         distribution = pkg_resources.get_distribution("AmberTools")
         return distribution.version
+
     except pkg_resources.DistributionNotFound:
-        return None
+        return _get_conda_list_package_versions().get("ambertools", None)
 
 
 def get_openeye_versions() -> Dict[str, str]:
