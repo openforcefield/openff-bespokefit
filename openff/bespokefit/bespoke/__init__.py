4b98ece248029a23ec6d0a250162e36947d6c808
Josh Horton <Josh.Horton@newcastle.ac.uk>
2021-07-21 15:47:32 +0100

    add schema serialize and deserialize util functions. Update notebook and add new notebook on local results.

--- openff/bespokefit/bespoke/__init__.py
+++ openff/bespokefit/bespoke/__init__.py
@@ -1,3 +1,4 @@
 from openff.bespokefit.bespoke.executor import Executor
+from openff.bespokefit.bespoke.utils import deserialize_schema, serialize_schema
 
-__all__ = [Executor]
+__all__ = [Executor, serialize_schema, deserialize_schema]
