3ce8b76dd09f9e676a86043d3f13ce5117467616
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-09-30 13:51:13 +0100

    Fix CI segmentation fault (#67)

--- openff/bespokefit/tests/__init__.py
+++ openff/bespokefit/tests/__init__.py
@@ -1,5 +1,21 @@
 from contextlib import contextmanager
 
+# A workaround for segfaults when using OE on GHA...
+try:
+    from openeye import oechem
+
+    if (
+        oechem.OEChemIsLicensed()
+        and oechem.OEGetMemPoolMode() == oechem.OEMemPoolMode_Default
+    ):
+
+        oechem.OESetMemPoolMode(
+            oechem.OEMemPoolMode_Mutexed | oechem.OEMemPoolMode_UnboundedCache
+        )
+
+except (ImportError, ModuleNotFoundError):
+    pass
+
 
 @contextmanager
 def does_not_raise():
