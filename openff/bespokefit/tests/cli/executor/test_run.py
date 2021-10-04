211308b29d38a624cd209a421e59e0cca7e95931
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-10-04 16:11:05 +0100

    Add run tests

---
+++ openff/bespokefit/tests/cli/executor/test_run.py
@@ -0,0 +1,71 @@
+import os
+
+import requests_mock
+from openff.toolkit.topology import Molecule
+
+from openff.bespokefit.cli.executor.run import run_cli
+from openff.bespokefit.executor.services import settings
+from openff.bespokefit.executor.services.coordinator.models import (
+    CoordinatorGETResponse,
+    CoordinatorGETStageStatus,
+    CoordinatorPOSTResponse,
+)
+
+
+def test_run(runner, tmpdir):
+    """Make sure to schema failures are cleanly handled."""
+
+    input_file_path = os.path.join(tmpdir, "mol.sdf")
+    Molecule.from_smiles("CC").to_file(input_file_path, "SDF")
+
+    with requests_mock.Mocker() as m:
+
+        m.post(
+            (
+                f"http://127.0.0.1:"
+                f"{settings.BEFLOW_GATEWAY_PORT}"
+                f"{settings.BEFLOW_API_V1_STR}/"
+                f"{settings.BEFLOW_COORDINATOR_PREFIX}"
+            ),
+            text=CoordinatorPOSTResponse(self="", id="1").json(),
+        )
+        m.get("http://127.0.0.1:8000/api/v1", text="")
+        m.get(
+            (
+                f"http://127.0.0.1:"
+                f"{settings.BEFLOW_GATEWAY_PORT}"
+                f"{settings.BEFLOW_API_V1_STR}/"
+                f"{settings.BEFLOW_COORDINATOR_PREFIX}/1"
+            ),
+            text=CoordinatorGETResponse(
+                id="1",
+                self="",
+                stages=[
+                    CoordinatorGETStageStatus(
+                        type="fragmentation", status="success", error=None, results=None
+                    )
+                ],
+            ).json(),
+        )
+
+        output = runner.invoke(
+            run_cli,
+            args=[
+                "--input",
+                input_file_path,
+                "--spec",
+                "debug",
+                "--directory",
+                "mock-directory",
+                "--n-fragmenter",
+                0,
+                "--n-qc-compute",
+                0,
+                "--n-optimizer",
+                0,
+                "--no-launch-redis",
+            ],
+        )
+
+    assert output.exit_code == 0
+    assert "workflow submitted: id=1" in output.output
