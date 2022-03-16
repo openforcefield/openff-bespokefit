418189fc07fb3687991927ffc50b9a5176bbf691
Josh Horton <Josh.Horton@newcastle.ac.uk>
2022-03-16 17:07:22 +0000

    Parallel torsiondrives (#143)
    * ready for new qcengine
    
    * add support for parallel torsiondrives and expose setting
    
    * flake8 fixes
    
    * Add simple test
    
    * PR feedback, update qcengine version pin
    
    * apply rules for auto tasks
    
    * change qcengine pin till new version
    
    * Fix test and update QCE pin
    
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>

---
+++ openff/bespokefit/executor/services/qcgenerator/qcengine.py
@@ -0,0 +1,85 @@
+from multiprocessing import Pool
+from typing import Dict, List, Union
+
+from qcelemental.models import FailedOperation
+from qcelemental.models.procedures import OptimizationResult, TorsionDriveInput
+from qcengine.config import TaskConfig
+from qcengine.procedures import register_procedure
+from qcengine.procedures.torsiondrive import TorsionDriveProcedure
+
+from openff.bespokefit.executor.services import current_settings
+
+
+def _divide_config(config: TaskConfig, n_workers: int) -> TaskConfig:
+    """
+    Divide the total resource for the worker between the parallel optimizations.
+    """
+    return TaskConfig(
+        ncores=int(config.ncores / n_workers),
+        memory=int(config.memory / n_workers),
+        retries=config.retries,
+        nnodes=1,
+    )
+
+
+class TorsionDriveProcedureParallel(TorsionDriveProcedure):
+    """
+    Override the _spawn_optimizations method of the basic torsiondrive procedure to allow for parallel optimizations
+    within one worker.
+    """
+
+    _defaults = {"name": "TorsionDriveParallel", "procedure": "torsiondrive"}
+
+    def _spawn_optimizations(
+        self,
+        next_jobs: Dict[str, List[List[float]]],
+        input_model: TorsionDriveInput,
+        config: TaskConfig,
+    ) -> Dict[str, List[Union[FailedOperation, OptimizationResult]]]:
+        """
+        Spawn parallel optimizations based on the number of next jobs and available workers.
+        """
+
+        settings = current_settings()
+        program = input_model.optimization_spec.keywords["program"]
+        opts_per_worker = settings.BEFLOW_QC_COMPUTE_WORKER_N_TASKS
+        if program == "psi4" and opts_per_worker == "auto":
+            # we recommend 8 cores per worker for psi4 from our qcfractal jobs
+            opts_per_worker = max([int(config.ncores / 8), 1])
+        elif opts_per_worker == "auto":
+            # for low cost methods like ani or xtb its often faster to not split the jobs
+            opts_per_worker = 1
+
+        n_jobs = sum([len(value) for value in next_jobs.values()])
+        if opts_per_worker > 1 and n_jobs > 1:
+            # split the resources based on the number of tasks
+            n_workers = int(min([n_jobs, opts_per_worker]))
+            opt_config = _divide_config(config=config, n_workers=n_workers)
+            with Pool(processes=n_workers) as pool:
+                tasks = {
+                    grid_point: [
+                        pool.apply_async(
+                            func=self._spawn_optimization,
+                            args=(grid_point, job, input_model, opt_config),
+                        )
+                        for job in jobs
+                    ]
+                    for grid_point, jobs in next_jobs.items()
+                }
+                return {
+                    grid_point: [grid_task.get() for grid_task in grid_tasks]
+                    for grid_point, grid_tasks in tasks.items()
+                }
+
+        else:
+            return {
+                grid_point: [
+                    self._spawn_optimization(grid_point, job, input_model, config)
+                    for job in jobs
+                ]
+                for grid_point, jobs in next_jobs.items()
+            }
+
+
+# register for local usage
+register_procedure(TorsionDriveProcedureParallel())
---
+++ openff/bespokefit/tests/executor/services/qcgenerator/test_qcengine.py
@@ -0,0 +1,68 @@
+import pytest
+from openff.toolkit.topology import Molecule
+from qcelemental.models.common_models import DriverEnum, Model
+from qcelemental.models.procedures import (
+    OptimizationSpecification,
+    QCInputSpecification,
+    TDKeywords,
+    TorsionDriveInput,
+)
+from qcengine.config import TaskConfig
+
+from openff.bespokefit.executor.services.qcgenerator.qcengine import (
+    TorsionDriveProcedureParallel,
+    _divide_config,
+)
+
+
+def test_divide_config():
+
+    task_config = TaskConfig(ncores=5, nnodes=1, memory=7, retries=1)
+    divided_config = _divide_config(task_config, 2)
+
+    assert divided_config.ncores == 2
+    assert divided_config.memory == 3
+    assert divided_config.nnodes == 1
+
+
+class TestTorsionDriveProcedureParallel:
+    @pytest.mark.parametrize(
+        "task_config",
+        [
+            TaskConfig(ncores=1, nnodes=1, memory=5, retries=1),
+            TaskConfig(ncores=2, nnodes=1, memory=5, retries=1),
+        ],
+    )
+    def test_spawn_optimizations(self, task_config, monkeypatch):
+        def mock_spawn_optimization(*args):
+            return args[2][0]
+
+        monkeypatch.setattr(
+            TorsionDriveProcedureParallel,
+            "_spawn_optimization",
+            mock_spawn_optimization,
+        )
+
+        procedure = TorsionDriveProcedureParallel()
+
+        molecule: Molecule = Molecule.from_smiles("CC")
+        molecule.generate_conformers(n_conformers=1)
+
+        input_schema = TorsionDriveInput(
+            keywords=TDKeywords(dihedrals=[(0, 1, 2, 3)], grid_spacing=[15]),
+            initial_molecule=[molecule.to_qcschema(conformer=0)],
+            input_specification=QCInputSpecification(
+                model=Model(method="hf", basis="6-31G*"),
+                driver=DriverEnum.gradient,
+            ),
+            optimization_spec=OptimizationSpecification(
+                procedure="geometric",
+                keywords={"program": "geometric"},
+            ),
+        )
+
+        results = procedure._spawn_optimizations(
+            {"(0,)": [[0], [1]], "(1,)": [[2], [3]]}, input_schema, task_config
+        )
+
+        assert results == {"(0,)": [0, 1], "(1,)": [2, 3]}
