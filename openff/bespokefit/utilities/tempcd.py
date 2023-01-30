21a8f0a8f492f07f47260d7d267cebbacd792078
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-01-30 17:01:02 +1100

    Simplify tempcd

--- openff/bespokefit/utilities/tempcd.py
+++ openff/bespokefit/utilities/tempcd.py
@@ -9,7 +9,7 @@ from openff.bespokefit.executor.services import current_settings
 
 
 @contextmanager
-def temporary_cd(path: Optional[str] = None, cleanup: Optional[bool] = None):
+def temporary_cd(path: Optional[str] = None):
     """Temporarily move the current working directory to the path specified.
 
     Parameters
@@ -17,29 +17,21 @@ def temporary_cd(path: Optional[str] = None, cleanup: Optional[bool] = None):
     path
         The path to CD into. If ``None`` or not specified, a temporary directory
         will be created.
-    cleanup
-        Whether the directory and its contents should be kept after the context
-        manager exits. If ``True``, the temporary directory and its contents
-        will be destroyed when the context manager exits; if ``False``, it will
-        be kept. If ``None`` or not specified, the directory is destroyed if
-        ``Path`` is not specified and the BEFLOW_KEEP_TMP_FILES setting is
-        ``False``.
     Returns
     -------
     """
+    # If the target path is "", we just want the current working directory.
     if path is not None and len(path) == 0:
         yield
         return
 
+    # If a path is not given, create a temporary directory
     if path is None:
-        path = tempfile.mkdtemp()
-        cleanup = (
-            (not current_settings().BEFLOW_KEEP_TMP_FILES)
-            if cleanup is None
-            else cleanup
-        )
+        path = tempfile.mkdtemp(dir=".")
+        print(f"created temporary directory {path}")
+        cleanup = not current_settings().BEFLOW_KEEP_TMP_FILES
     else:
-        cleanup = False if cleanup is None else cleanup
+        cleanup = False
 
     old_directory = os.getcwd()
 
@@ -49,5 +41,7 @@ def temporary_cd(path: Optional[str] = None, cleanup: Optional[bool] = None):
 
     finally:
         os.chdir(old_directory)
+        # If we created a temporary directory, clean it up
         if cleanup:
+            print(f"cleaning up temporary directory {path}")
             shutil.rmtree(path)
