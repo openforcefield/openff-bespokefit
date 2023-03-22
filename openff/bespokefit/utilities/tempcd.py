309dc71c862254269196a87c0b1a16535ca3f7c3
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-03-22 18:07:56 +1100

    Update temporary_cd to pass tests

--- openff/bespokefit/utilities/tempcd.py
+++ openff/bespokefit/utilities/tempcd.py
@@ -2,34 +2,48 @@
 import os
 import shutil
 import tempfile
+from pathlib import Path
 from contextlib import contextmanager
-from typing import Optional
+from typing import Optional, Union
 
 from openff.bespokefit.executor.services import current_settings
 
 
 @contextmanager
-def temporary_cd(path: Optional[str] = None):
-    """Temporarily move the current working directory to the path specified.
+def temporary_cd(path: Optional[Union[str, Path]] = None, parents=False):
+    """
+    Context manager to move the current working directory to the path specified.
+
+    If no path is given or the path does not exist, a temporary directory will
+    be created. This temporary directory and its contents will be deleted when
+    the context manager exits.
 
     Parameters
     ----------
     path
         The path to CD into. If ``None`` or not specified, a temporary directory
-        will be created.
-    Returns
-    -------
+        will be created. If specified but the path does not exist, a temporary
+        directory with that name will be created.
+    parents
+        If parents is ``True``, any missing parents of this path are created as
+        needed; they are created with the default permissions without taking
+        mode into account. If ``False``, missing parents will result in a
+        :py:exc:`FileNotFoundError` error.
     """
-    # If the target path is "", we just want the current working directory.
-    if path is not None and len(path) == 0:
-        yield
-        return
+    path: Optional[Path] = None if path is None else Path(path)
+
+    # Decide whether to clean up based on bespokefit settings
+    cleanup = not current_settings().BEFLOW_KEEP_TMP_FILES
 
     # If a path is not given, create a temporary directory
     if path is None:
-        path = tempfile.mkdtemp(dir=".")
+        path = Path(tempfile.mkdtemp(dir="."))
         print(f"created temporary directory {path}")
-        cleanup = not current_settings().BEFLOW_KEEP_TMP_FILES
+    # If a path is given but does not already exist, create it
+    elif not path.exists():
+        path.mkdir(parents=parents)
+        print(f"created temporary directory {path}")
+    # If we didn't create the path, do NOT clean it up
     else:
         cleanup = False
 
@@ -41,7 +55,7 @@ def temporary_cd(path: Optional[str] = None):
 
     finally:
         os.chdir(old_directory)
-        # If we created a temporary directory, clean it up
+        # If we created the directory, clean it up
         if cleanup:
             print(f"cleaning up temporary directory {path}")
             shutil.rmtree(path)
