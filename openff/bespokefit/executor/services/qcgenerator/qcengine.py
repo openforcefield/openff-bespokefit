dbcd9b4e1b7a0fb41f2e329179de8f492d0a8bc9
Josh Horton <Josh.Horton@newcastle.ac.uk>
2023-07-20 15:44:35 +0100

    make cli workers use the solo celery pool, enable parallel torsiondrives

--- openff/bespokefit/executor/services/qcgenerator/qcengine.py
+++ openff/bespokefit/executor/services/qcgenerator/qcengine.py
@@ -1,6 +1,8 @@
-from multiprocessing import Pool
+import logging
+from multiprocessing import Pool, current_process
 from typing import Dict, List, Union
 
+from celery.utils.log import get_task_logger
 from qcelemental.models import FailedOperation
 from qcelemental.models.procedures import OptimizationResult, TorsionDriveInput
 from qcengine.config import TaskConfig
@@ -8,6 +10,8 @@ from qcengine.procedures import register_procedure
 from qcengine.procedures.torsiondrive import TorsionDriveProcedure
 
 from openff.bespokefit.executor.services import current_settings
+
+_task_logger: logging.Logger = get_task_logger(__name__)
 
 
 def _divide_config(config: TaskConfig, n_workers: int) -> TaskConfig:
@@ -43,6 +47,12 @@ class TorsionDriveProcedureParallel(TorsionDriveProcedure):
         settings = current_settings()
         program = input_model.optimization_spec.keywords["program"]
         opts_per_worker = settings.BEFLOW_QC_COMPUTE_WORKER_N_TASKS
+        # we can only split the tasks if the celery worker is the main process so if not set back to 1
+        if not current_process().name == "MainProcess":
+            _task_logger.info(
+                "Worker not in main process limited to serial optimisations."
+            )
+            opts_per_worker = 1
         if program == "psi4" and opts_per_worker == "auto":
             # we recommend 8 cores per worker for psi4 from our qcfractal jobs
             opts_per_worker = max([int(config.ncores / 8), 1])
@@ -55,6 +65,9 @@ class TorsionDriveProcedureParallel(TorsionDriveProcedure):
             # split the resources based on the number of tasks
             n_workers = int(min([n_jobs, opts_per_worker]))
             opt_config = _divide_config(config=config, n_workers=n_workers)
+            _task_logger.info(
+                f"Starting {n_workers} parallel optimisations for {n_jobs} jobs"
+            )
             with Pool(processes=n_workers) as pool:
                 tasks = {
                     grid_point: [
